d3c48c2723d35d81ca38275b64981b2b362a48a9
vishaltanawade688@gmail.com <vishaltanawade688@gmail.com>
2022-05-23 16:22:50 +0530

    Uninstall (Comment)

--- MyLogDetailsService/Service1.cs
+++ MyLogDetailsService/Service1.cs
@@ -19,6 +19,8 @@ namespace MyLogDetailsService
         }
         // Run this command in VS IDE cmd in below folder
         //installUtil MyLogDetailsService.exe
+        // To unistall use below code
+        //installUtil -u MyLogDetailsService.exe
         protected override void OnStart(string[] args)
          {
             if (!File.Exists(@"D:\Cognizant\Cognizant Internship Data\Stage 3\20May\LogDetailsFolder\Log.txt"))
